a7c249c08cc59f913ccf6996684f0abc3e878af4
RemonShehata <RemonGerges94@gmail.com>
2020-10-21 00:46:24 +0200

    Initial problem

---
+++ Strategy Pattern/SimUDuck/src/Duck.java
@@ -0,0 +1,12 @@
+public abstract class Duck {
+
+    void quck() {
+        System.out.println("Duck is quacking");
+    }
+
+    void swim() {
+        System.out.println("Duck is swimming");
+    }
+
+    abstract void display();
+}
