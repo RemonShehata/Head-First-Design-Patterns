72080fc964cfeec7fcc639679521db810113365e
RemonShehata <RemonGerges94@gmail.com>
2020-10-21 01:47:52 +0200

    Set the behavior dynamically Added a new type of Duck Added a new fly behavior

--- Strategy Pattern/SimUDuck/src/Duck.java
+++ Strategy Pattern/SimUDuck/src/Duck.java
@@ -3,6 +3,14 @@ public abstract class Duck {
     FlyBehavior flyBehavior;
     QuackBehavior quackBehavior;
 
+    public void setFlyBehavior(FlyBehavior fb) {
+        flyBehavior = fb;
+    }
+
+    public void setQuackBehavior(QuackBehavior qb) {
+        quackBehavior = qb;
+    }
+
     void swim() {
         System.out.println("Duck is swimming");
     }
