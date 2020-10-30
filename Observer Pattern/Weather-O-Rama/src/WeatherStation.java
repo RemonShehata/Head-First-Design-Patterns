7c917d14fe8474646a26b93825a7812ec942d813
RemonShehata <RemonGerges94@gmail.com>
2020-10-30 21:02:52 +0200

    Implemented the rest of the displays

--- Observer Pattern/Weather-O-Rama/src/WeatherStation.java
+++ Observer Pattern/Weather-O-Rama/src/WeatherStation.java
@@ -3,6 +3,8 @@ public class WeatherStation {
     public static void main(String[] args) {
         WeatherData weatherData = new WeatherData();
         CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
+        ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
 
         weatherData.setMeasurements(80, 65, 30.4f);
         weatherData.setMeasurements(82, 70, 29.2f);
